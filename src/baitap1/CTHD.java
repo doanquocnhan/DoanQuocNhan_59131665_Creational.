c67574183b84d012320845fdaa5f3dcd8aac9435
doanquocnhan <58899942+doanquocnhan@users.noreply.github.com>
2020-05-11 21:35:59 +0700

    v4.0

--- src/baitap1/CTHD.java
+++ src/baitap1/CTHD.java
@@ -10,5 +10,58 @@ package baitap1;
  * @author Nhan
  */
 public class CTHD {
+    String SanPham;
+    int Soluong;
+    float DonGia, Chietkhau;
+
+    public CTHD(String SanPham, int Soluong, float DonGia, float Chietkhau) {
+        this.SanPham = SanPham;
+        this.Soluong = Soluong;
+        this.DonGia = DonGia;
+        this.Chietkhau = Chietkhau;
+    }
+
+    public CTHD() {
+    }
+
+    public String getSanPham() {
+        return SanPham;
+    }
+
+    public void setSanPham(String SanPham) {
+        this.SanPham = SanPham;
+    }
+
+    public int getSoluong() {
+        return Soluong;
+    }
+
+    public void setSoluong(int Soluong) {
+        this.Soluong = Soluong;
+    }
+
+    public float getDonGia() {
+        return DonGia;
+    }
+
+    public void setDonGia(float DonGia) {
+        this.DonGia = DonGia;
+    }
+
+    public float getChietkhau() {
+        return Chietkhau;
+    }
+
+    public void setChietkhau(float Chietkhau) {
+        this.Chietkhau = Chietkhau;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "\n Chi Tiet Hoa don{" + "Sảm phẩm :" + SanPham + ", Số Lượng: " + Soluong + ", Đơn Giá: " + DonGia + ", Chiết Khấu: " + Chietkhau + '}';
+    }
+    
+          
     
 }
